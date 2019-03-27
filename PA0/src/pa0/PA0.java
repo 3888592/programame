be830e4f430ebd2e8b962202c2b237b740b79812
Mathi <trabajomathi@hotmail.com>
2019-03-27 09:31:31 +0100

    Estructura PA0

---
+++ PA0/src/pa0/PA0.java
@@ -0,0 +1,34 @@
+
+package pa0;
+
+public class PA0 {
+
+    public static void main(String[] args) {
+        boolean finalizar;
+        
+        do{
+            String entrada = leerEntrada();
+            if(!(finalizar = finalDelJuego())) {
+                logica();
+                escribirSalida();
+            }
+        }while(!finalizar);
+    }
+//jugador1
+    private static String leerEntrada() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+//jugador2
+    private static boolean finalDelJuego() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void logica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void escribirSalida() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
